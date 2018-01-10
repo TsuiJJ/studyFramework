ae99fe03de402ea9ad9a31d00de9a29d23e2e648
cmomacpro <xujijun@jd.com>
2018-01-10 14:17:15 +0800

    test

---
+++ frameworkTest/aa/person.m
@@ -0,0 +1,15 @@
+//
+//  person.m
+//  frameworkTest
+//
+//  Created by cmomacpro on 2017/12/29.
+//  Copyright © 2017年 cmomacpro. All rights reserved.
+//
+
+#import "person.h"
+
+@implementation person
+-(void)sayhello:(NSString *)name{
+    NSLog(@"hello:%@", name);
+}
+@end
