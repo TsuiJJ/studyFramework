d1c659b0cf618b5f8f45fd5e8f25882fc2fa667a
cmomacpro <xujijun@jd.com>
2018-01-10 14:14:32 +0800

    add

---
+++ frameworkTest/person.h
@@ -0,0 +1,13 @@
+//
+//  person.h
+//  frameworkTest
+//
+//  Created by cmomacpro on 2017/12/29.
+//  Copyright © 2017年 cmomacpro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface person : NSObject
+-(void)sayhello:(NSString *)name;
+@end
